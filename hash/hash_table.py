16ae4209f9042a20d9ec4e66c70bf1c254818e4d
roflalex <C24319666@MyTUDublin.ie>
2026-04-23 18:10:30 +0100

    Add hashing methods to HashTable

--- hash/hash_table.py
+++ hash/hash_table.py
@@ -3,3 +3,22 @@ class HashTable:
         self.size = size
         self.table = [[] for _ in range(size)]
         self.collisions = 0
+
+    def hash(self,key):
+        return hash(key) % self.size
+    
+    def insert(self,movie):
+        key = movie["title"]
+        index = self.hash(key)
+
+        if len(self.table[index]) > 0:
+            self.collisions +=1
+
+        self.table[index].append(movie)
+    def search(self,title):
+        index = self.hash(title)
+        
+        for movie in self.table[index]:
+            if movie["title"] == title:
+                return movie
+        return None
